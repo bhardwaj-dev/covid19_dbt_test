877f2574aec421501932fa91b1488c10dceb82c7
nitesh140892@gmail.com <nitesh140892@gmail.com>
2021-12-21 16:46:18 +0000

    second commit

--- models/dbt_test/date.sql
+++ models/dbt_test/date.sql
@@ -3,7 +3,7 @@
 with test as (
     select 
     _row as id,
-    DATE
+  to_date(to_char(DATE))  date
     from {{ source('nitesh_source', 'COVID_19_INDONESIA_NITESH_BHARDWAJ') }}
 )
-select * from test
+select id, TRY_TO_DATE(date) date from test
